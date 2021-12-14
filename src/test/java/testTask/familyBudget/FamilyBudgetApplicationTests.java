2265b8dca7ac576bb96bbc1411f6a9226957f8e0
Dmitriy_Yavorskiy <dyav2017@gmail.com>
2021-12-14 15:44:55 +0200

    Base commit. Start process.

---
+++ src/test/java/testTask/familyBudget/FamilyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package testTask.familyBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
